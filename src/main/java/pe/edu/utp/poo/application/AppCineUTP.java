7b93392b72e1d58fdac453d60ab99b4d46c837eb
TIBURON\rodol <rodolfopiscisggt@gmail.com>
2024-11-02 23:39:14 -0500

    yo ise login

--- src/main/java/pe/edu/utp/poo/application/AppCineUTP.java
+++ src/main/java/pe/edu/utp/poo/application/AppCineUTP.java
@@ -4,6 +4,7 @@
 
 package pe.edu.utp.poo.application;
 
+import pe.edu.utp.poo.application.ui.Acceso;
 import pe.edu.utp.poo.application.ui.UIMainMenu;
 
 /**
@@ -17,7 +18,8 @@ public class AppCineUTP {
      */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(() -> {
-            UIMainMenu uiMain = new UIMainMenu();
+            //UIMainMenu uiMain = new UIMainMenu();
+            Acceso uiMain = new Acceso();
             uiMain.setVisible(true);
             uiMain.setLocationRelativeTo(null);
         });
